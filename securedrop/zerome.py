edf464b06e22436a046f361cff4aeb8e0b048636
Jack Singleton <git@jacksingleton.com>
2014-07-26 18:27:00 -0700

    add functions to zero out a unicode string

--- securedrop/zerome.py
+++ securedrop/zerome.py
@@ -9,24 +9,57 @@ def zerome_string(string):
         temp = "finding offset"
         raw_temp = ctypes.string_at(id(temp), sys.getsizeof(temp))
         return raw_temp.find(temp)
-    def location(s):
-        return id(s) + header_size()
-    def size(s):
-        return sys.getsizeof(s) - header_size()
+    location = id(string) + header_size()
+    size = sys.getsizeof(string) - header_size()
 
-    ctypes.memset(location(string), 0, size(string))
+    ctypes.memset(location, 0, size)
+
+
+class _PyUnicodeObjectStruct(ctypes.Structure):
+    '''Include/object.h:77
+    #define PyObject_HEAD                   \
+        _PyObject_HEAD_EXTRA                \
+        Py_ssize_t ob_refcnt;               \
+        struct _typeobject *ob_type;
+
+    Include/unicodeobject.h:413
+    typedef struct {
+        PyObject_HEAD
+        Py_ssize_t length;          /* Length of raw Unicode data in buffer */
+        Py_UNICODE *str;            /* Raw Unicode buffer */
+        long hash;                  /* Hash value; -1 if not set */
+        PyObject *defenc;           /* (Default) Encoded version as Python
+                                    string, or NULL; this is used for
+                                    implementing the buffer protocol */
+    } PyUnicodeObject;
+    '''
+    _fields_ = [
+        ('ob_refcnt', ctypes.c_size_t),
+        ('ob_type', ctypes.c_void_p),
+        ('length', ctypes.c_size_t),
+        ('str', ctypes.POINTER(ctypes.c_wchar)),
+        ('hash', ctypes.c_long),
+        ('defenc', ctypes.c_void_p)
+    ]
+
+def _unicode_bytes_per_char():
+    '''Python can be compiled for either "thin" or "wide" unicode support.
+    A thin build uses two bytes to store unicode characters, while the
+    wide build uses four. Not all unicode characters can be represented
+    in two byes, so trying to create a character from a high unicode
+    value tells us if we have a wide or thin build.
+    https://docs.python.org/2/library/functions.html#unichr
+    '''
+    try:
+        unichr(0x10000)
+        return 4 # wide build -> UCS4 -> 4 bytes
+    except ValueError:
+        return 2 # thin build -> UCS2 -> 2 bytes
 
 def zerome_unicode_string(string):
-    def header_size():
-        temp = "finding offset"
-        raw_temp = ctypes.string_at(id(temp), sys.getsizeof(temp))
-        print '\nraw_temp', repr(raw_temp)
-        return raw_temp.find(temp)
-    def location(s):
-        return id(s) + header_size()
-    def size(s):
-        return sys.getsizeof(s) - header_size()
+    string_pointer = ctypes.c_void_p(id(string))
+    c_type = ctypes.POINTER(_PyUnicodeObjectStruct)
+    string_struct = ctypes.cast(string_pointer, c_type).contents
+    raw_string_size = string_struct.length * _unicode_bytes_per_char()
 
-    ctypes.memset(location(string), 0, size(string))
-
-    print 'after zerome', repr(string)
+    ctypes.memset(string_struct.str, 0, raw_string_size)
